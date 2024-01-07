f29cf9d7937fcf84051284b960d1aca5ed95b998
AliMaazKhan <77734248+AliMaazKhan@users.noreply.github.com>
2024-01-07 12:11:40 +0530

    completed varibles , created dataTypes file

--- Basics/test.js
+++
@@ -1,3 +0,0 @@
-console.log("Ali")
-// this is the change
-console.log("please git add this :( ")
---
+++ Basics/varibles.js
@@ -0,0 +1,22 @@
+const accountID = 2345234;
+let accountEmail = "alimaaz@coder.com";
+var accountPassword = "13423";
+accountCity = "Mumbai";
+let accountState;
+
+// accountID = 23; not allowed
+
+{
+
+}
+
+accountEmail = "github@support.com";
+accountPassword ="69";
+accountCity = "Delhi";
+console.log(accountID); 
+
+/* Prefer no to use var 
+because of issues with the block
+*/
+
+console.table([accountEmail,accountPassword,accountID,accountCity,accountState]);
