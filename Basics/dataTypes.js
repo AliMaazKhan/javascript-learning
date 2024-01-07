4f12fa6fe485ad0874b24ead5279063dee763e2b
AliMaazKhan <77734248+AliMaazKhan@users.noreply.github.com>
2024-01-07 12:28:08 +0530

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); we are using node js, not browser
+
+console.log(3 + 3) 
+console.log("ali")
+
+let name = "ali"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to the power of 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
