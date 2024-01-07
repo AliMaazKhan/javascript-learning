76154cf659ff1d0eb7bc03c4c5c7a840a7537b54
AliMaazKhan <77734248+AliMaazKhan@users.noreply.github.com>
2024-01-07 12:41:55 +0530

    Converson of data types

---
+++ Basics/conversions.js
@@ -0,0 +1,30 @@
+let score = "tomm"
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "a"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" =>true
+
+let someNumber = 55
+
+let StringNumber = String(someNumber)
+
+console.log(typeof StringNumber);
+console.log(StringNumber);
