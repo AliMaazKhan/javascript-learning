8014f67a81531a47edb133f49cc9d3abb4d18211
AliMaazKhan <77734248+AliMaazKhan@users.noreply.github.com>
2024-01-07 20:20:10 +0530

    Completed conversions

--- Basics/conversions.js
+++ Basics/conversions.js
@@ -1,5 +1,5 @@
 let score = "tomm"
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
@@ -26,5 +26,45 @@ let someNumber = 55
 
 let StringNumber = String(someNumber)
 
-console.log(typeof StringNumber);
-console.log(StringNumber);
+// console.log(typeof StringNumber);
+// console.log(StringNumber);
+
+
+
+//****************************  Operations  ********************************************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log( 2 % 2);
+
+
+
+let str1 = "hello "
+let str2 = "ali"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(((2 + 3) * 5) / 2);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
