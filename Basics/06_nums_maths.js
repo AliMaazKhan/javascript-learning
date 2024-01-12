fa55afbf43d867e13432c90c3e9a1b9ff4000676
AliMaazKhan <77734248+AliMaazKhan@users.noreply.github.com>
2024-01-12 22:52:57 +0530

    Math and Number

---
+++ Basics/06_nums_maths.js
@@ -0,0 +1,27 @@
+const score = 500
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(1)); 
+
+const otherNumber = 234.4324
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.7));
+// console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
