7e850225832722b4c99b728468b029a7682025c0
root <root@DESKTOP-20NJ6QE.>
2024-03-13 21:03:35 +0530

    add the file

---
+++ 01Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12532;
+let accountEmail = "alimaaz8998@gmail.com";
+var accountPassword = "235234";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2; not allowed
+
+
+accountEmail = "hc@gaim.com";
+accountPassword = "087234";
+accountCity = "delhi";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
